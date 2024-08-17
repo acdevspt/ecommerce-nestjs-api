00f708f8e4ca00aa100ff1cae377835be86c2c67
André <casal.andre00@gmail.com>
2024-08-17 18:44:53 +0100

    improved the api design of users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersDto } from './dto/users.dto';
 
@@ -6,15 +6,24 @@ import { UsersDto } from './dto/users.dto';
 export class UsersController {
     constructor(private userService: UsersService) {}
 
+    // create a new order
     @Post()
     @HttpCode(HttpStatus.CREATED)
     async addUser(@Body() dto: UsersDto) {
         return this.userService.addUser(dto)
     }
 
+    // the the orders of all users
     @Get()
     @HttpCode(HttpStatus.CREATED)
     async getUsers() {
         return this.userService.getUsers()
+    }
+
+    // retrieve the user orders
+    @Get(":user_uuid/orders")
+    @HttpCode(HttpStatus.CREATED)
+    async getUserOrders(@Param("user_uuid") userUuid: string) {
+        return this.userService.getUserOrders(userUuid)
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UsersDto } from './dto/users.dto';
+import { Timestamp } from 'rxjs';
 
 @Injectable()
 export class UsersService {
@@ -25,6 +26,14 @@ export class UsersService {
     async getUsers() {
         try {
             return await this.prisma.users.findMany()
+        } catch {
+            throw new InternalServerErrorException("Something when wrong!")
+        }
+    }
+
+    async getUserOrders(user_uuid: string) {
+        try {
+            return await this.prisma.$queryRaw`SELECT order_items.* FROM order_items JOIN orders ON order_items.order_uuid = orders.uuid WHERE orders.user_uuid = ${user_uuid}`
         } catch {
             throw new InternalServerErrorException("Something when wrong!")
         }
